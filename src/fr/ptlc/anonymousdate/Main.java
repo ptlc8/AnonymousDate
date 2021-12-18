68ff6e99dbd7b2d105565ec1395188d72ee65594
PTLC_ <ptlc.ptlc.ptlc@gmail.com>
2021-12-18 19:16:42 +0100

    ajout d'une demande de token via token.txt

--- src/fr/ptlc/anonymousdate/Main.java
+++ src/fr/ptlc/anonymousdate/Main.java
@@ -1,5 +1,10 @@
 package fr.ptlc.anonymousdate;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.JDA;
@@ -15,18 +20,30 @@ public class Main {
 	private static final String creationDate = "14/02/2021";
 	private static final String date = "14/02/2021";
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		JDA jda = null;
 		System.out.println("AnonymousDate " + getVersion() + ", par Ambi/PTLC_, le " + getDate() + ", pour 💕 Love Dating 💕, vive l'amour !");
+
+		File tokenFile = new File("token.txt");
+		String token = null;
+		if (!tokenFile.exists())
+			tokenFile.createNewFile();
+		BufferedReader tokenReader = new BufferedReader(new FileReader(tokenFile));
+		token = tokenReader.readLine();
+		tokenReader.close();
 		
-		try {
-			jda = JDABuilder.createDefault("ODEwMjk3NDc0MDE4NzA1NDI5.YChmVA.luCxAhesGBrkd97z1LTHeqsPBVY").build();
-		} catch (LoginException e) {
-			e.printStackTrace();
+		if (token == null) {
+			System.out.println("Veuillez mettre le token du bot dans token.txt");
+		} else {
+			try {
+				jda = JDABuilder.createDefault(token).build();
+			} catch (LoginException e) {
+				e.printStackTrace();
+			}
+			if (isBeta()) jda.getPresence().setPresence(OnlineStatus.DO_NOT_DISTURB, Activity.of(ActivityType.DEFAULT, " en cours de mise à jour"));
+			else jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.of(ActivityType.LISTENING, " ce que tu as transmettre"));
+			jda.addEventListener(new CommandsListener());
 		}
-		if (isBeta()) jda.getPresence().setPresence(OnlineStatus.DO_NOT_DISTURB, Activity.of(ActivityType.DEFAULT, " en cours de mise à jour"));
-		else jda.getPresence().setPresence(OnlineStatus.ONLINE, Activity.of(ActivityType.LISTENING, " ce que tu as transmettre"));
-		jda.addEventListener(new CommandsListener());
 	}
 	
 	public static String getVersion() {
